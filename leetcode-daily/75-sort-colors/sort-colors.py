3fb7f82c45de39324893b1eb4e1b2c6e72deda87
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-12 15:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.08%) - LeetSync

--- leetcode-daily/75-sort-colors/sort-colors.py
+++ leetcode-daily/75-sort-colors/sort-colors.py
@@ -1,4 +1,12 @@
+from typing import List
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        counts = [0, 0, 0]
+        for num in nums:
+            counts[num] += 1
         
+        zeroes, ones, twos = counts
+        nums[:zeroes] = [0] * zeroes
+        nums[zeroes : zeroes + ones] = [1] * ones
+        nums[zeroes + ones : zeroes + ones + twos] = [2] * twos
