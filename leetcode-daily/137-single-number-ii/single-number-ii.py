77e29a46187776bdba3f506137d248c6446fa6fc
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-14 18:52:25 +0530

    Time: 3 ms (68.00%) | Memory: 19.6 MB (18.98%) - LeetSync

---
+++ leetcode-daily/137-single-number-ii/single-number-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        k=defaultdict(int)
+        for num in nums:
+            k[num]+=1
+        for key,val in k.items():
+            if val==1:
+                return key
