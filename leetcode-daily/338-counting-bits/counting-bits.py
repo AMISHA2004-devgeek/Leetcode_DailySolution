74223eafe8c4dbefa478f90884879fb73e0f35c7
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-14 18:24:18 +0530

    Time: 5 ms (57.26%) | Memory: 18.7 MB (22.76%) - LeetSync

--- leetcode-daily/338-counting-bits/counting-bits.py
+++ leetcode-daily/338-counting-bits/counting-bits.py
@@ -2,7 +2,7 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         k=[0]*(n+1)
         for i in range(n+1):
-            k[i]=bin(i).count('1')
+            k[i]=k[i>>1]+(i&1)
         return k
 
 
