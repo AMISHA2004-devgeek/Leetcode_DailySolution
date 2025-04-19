bedf8b5ebccd0edcfe8c488a942e6aeba32e6893
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-19 14:06:02 +0530

    Time: 10 ms (75.00%) | Memory: 17.8 MB (64.44%) - LeetSync

--- leetcode-daily/242-valid-anagram/valid-anagram.py
+++ leetcode-daily/242-valid-anagram/valid-anagram.py
@@ -1,13 +1,13 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         count=[0]*26
-        for char in s:
-            count[ord(char)-ord('a')]+=1
-        for x in t:
-            count[ord(x)-ord('a')]-=1
-
+        for ch in s:
+            count[ord(ch)-ord('a')]+=1
+        for r in t:
+            count[ord(r)-ord('a')]-=1
         return count==[0]*26
-            
+
 
 
         
