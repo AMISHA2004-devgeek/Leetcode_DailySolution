c65557650d72f36ecf13f2809405fa401939d2dd
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-23 08:33:01 +0530

    Time: 3 ms (65.55%) | Memory: 19.8 MB (14.18%) - LeetSync

--- leetcode-daily/136-single-number/single-number.py
+++ leetcode-daily/136-single-number/single-number.py
@@ -1,11 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        k=defaultdict(int)
-        for num in nums:
-            k[num]+=1
-        
-        for key,val in k.items():
-            if val==1:
-                return key
-
+        k=Counter(nums)
+        for k ,v in k.items():
+            if v==1:
+                return k 
         
