843d0683990203eb8200b103d43f32ed68de1130
AMISHA_KUMARI <amishak9824@gmail.com>
2025-04-24 23:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (72.07%) - LeetSync

--- leetcode-daily/88-merge-sorted-array/merge-sorted-array.py
+++ leetcode-daily/88-merge-sorted-array/merge-sorted-array.py
@@ -2,7 +2,7 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         i=m-1
         j=n-1
-        k=m+n-1
+        k=len(nums1)-1
         while i>=0 and j>=0:
             if nums2[j]>nums1[i]:
                 nums1[k]=nums2[j]
