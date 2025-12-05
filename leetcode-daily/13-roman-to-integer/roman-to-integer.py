9d2dfc4163e88454bd543c794c883a4e6fd15365
AMISHA_KUMARI <amishak9824@gmail.com>
2025-12-05 09:48:39 +0530

    Time: 3 ms (92.41%) | Memory: 12.5 MB (13.13%) - LeetSync

--- leetcode-daily/13-roman-to-integer/roman-to-integer.py
+++ leetcode-daily/13-roman-to-integer/roman-to-integer.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def romanToInt(self, s):
-        roman_values = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}   
-        t=0
-        p=0
+        roman_values = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}  
+        t=p=0
+        curr=0
         for ch in reversed(s):
             curr=roman_values[ch]
-            if curr <p:
+            if curr<p:
                 t-=curr
             else:
                 t+=curr
