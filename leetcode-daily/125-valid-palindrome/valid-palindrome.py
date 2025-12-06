279b7f4720f06b17af6e7c39e002d606a2c05595
AMISHA_KUMARI <amishak9824@gmail.com>
2025-12-06 10:14:07 +0530

    Time: 3 ms (99.03%) | Memory: 18 MB (82.43%) - LeetSync

--- leetcode-daily/125-valid-palindrome/valid-palindrome.py
+++ leetcode-daily/125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        r=''.join(s).lower()
-        k=""
-        for ch in r:
-            if ch.isalnum():
-                k+=ch
-        return k==k[::-1]
+        r=s.lower()
+        i=0
+        j=len(r)-1
+        while i<j:
+            if not r[i].isalnum():
+                i+=1
+                continue
+            if not r[j].isalnum():
+                j-=1
+                continue
+            if r[i]!=r[j]:
+                return False
+            else:
+                i+=1
+                j-=1
+        return True
+       
+                
         
 
         
